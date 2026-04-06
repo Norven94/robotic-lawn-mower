2cd3f086dbd80137c8d1c9db8728d60a954697c4
Emilybjernetun <emilybj@chalmers.se>
2026-04-06 18:42:38 +0200

    bytte input till inquirer

--- cli.py
+++ cli.py
@@ -1,19 +1,23 @@
-#Fråga användaren efter ett val av körläge
-while True:
-    print("Välkommen till robotgräskliparen!")
-    print("Välj körläge:")
-    print("1. Slumpmässig rörelse")
-    print("2. Utsatt slinga")
+import inquirer
 
-    val = input("Välj körläge (1 eller 2): ") #input= stannar programmet och väntar på att användaren ska skriva in något
-    #användaren väljer om de vill ha 1 eller 2
-    if val == "1":
+while True:
+    print("Välkommen till robotgräsklipparen!")
+
+    # Skapa listan med val
+    questions = [
+        inquirer.List(
+            "choice",
+            message="Välj körläge",
+            choices=["Slumpmässig rörelse", "Utsatt slinga"],
+        )        
+    ]
+
+    # Hämta svaret från användaren (Se till att denna ligger rakt under 'questions')
+    answer = inquirer.prompt(questions)["choice"]
+
+    if answer == "Slumpmässig rörelse":
         print("Du har valt slumpmässig rörelse.")
-        #lägga till kod för själva körningen här?
-        break #hoppar ur loopen
-    elif val == "2": #elif = else if, används för att kolla flera olika villkor
+        break
+    elif answer == "Utsatt slinga":
         print("Du har valt utsatt slinga.")
-        #lägga till kod för själva körningen här?
-        break #hoppar ur loopen
-    else: #om användaren inte väljer 1 eller 2, skriv ut ett felmeddelande och fortsätt loopen eftersom den inte bryts (break)
-        print("Ogiltigt val, vänligen försök igen!")
+        break
