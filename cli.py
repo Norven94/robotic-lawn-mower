03c08427d70b571285fc205bd481778c93d191b0
Emilybjernetun <emilybj@chalmers.se>
2026-04-06 18:52:20 +0200

    Add comments for clarity in cli.py
    Added comments in Swedish to clarify code functionality.

--- cli.py
+++ cli.py
@@ -1,23 +1,26 @@
 import inquirer
 
 while True:
+    #skriver ut en välkomsthälsning varje gång loopen börjar om
     print("Välkommen till robotgräsklipparen!")
 
     # Skapa listan med val
     questions = [
         inquirer.List(
-            "choice",
-            message="Välj körläge",
-            choices=["Slumpmässig rörelse", "Utsatt slinga"],
+            "choice", #namnet på variablen där svaret sparas temporärt
+            message="Välj körläge", #text som visas för användare
+            choices=["Slumpmässig rörelse", "Utsatt slinga"], #de alternativ användaren kan välja mellan
         )        
     ]
 
-    # Hämta svaret från användaren (Se till att denna ligger rakt under 'questions')
+    #hämta svaret från användaren (pilar + enter)
+    #hämtar svaret via nyckeln - "choice"
     answer = inquirer.prompt(questions)["choice"]
 
+    #logik för att hantera valen
     if answer == "Slumpmässig rörelse":
-        print("Du har valt slumpmässig rörelse.")
-        break
+        print("Du har valt slumpmässig rörelse.") #skriver ut dte som står i parantesen
+        break #bryter loopen och avslutar programmet efter valet
     elif answer == "Utsatt slinga":
         print("Du har valt utsatt slinga.")
         break
