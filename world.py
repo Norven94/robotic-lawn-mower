a5c397c91b73bb17c6c3350ec2e521793a0817a2
Oskar Anderberg <73995193+Norven94@users.noreply.github.com>
2026-04-16 15:35:25 +0200

    move out obstacles

--- world.py
+++ world.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING
 
+from matplotlib.axes import Axes
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.lines import Line2D
@@ -13,7 +14,7 @@ from lawn import Lawn
 from utilities import getLawnPoints, sync_linewidth_to_data, LawnPointsOption
 
 import settings 
-from obstacles import rectangleobstacles
+from obstacles import Obstacles
 
 if TYPE_CHECKING:
 	from controller import Controller
@@ -21,28 +22,17 @@ if TYPE_CHECKING:
 
 @dataclass
 class World:
-	# min_x: float = settings.LAWN_MIN_X
-	# max_x: float = settings.LAWN_MAX_X
-	# min_y: float = settings.LAWN_MIN_Y
-	# max_y: float = settings.LAWN_MAX_Y
 	lawn = Lawn()
-	
+	obstacles = Obstacles()
 	mowed_points: list[tuple[float, float]] = field(default_factory=list, init=False)
-	obstacles: list = field(default_factory=list, init=False)
-
-	#List for obstacles
-	def __post_init__ (self):
-		house = rectangleobstacles( settings.OFF_SET_PLOT_X, settings.OFF_SET_PLOT_Y, settings.HOUSE_SIZE )
-		self.obstacles.append(house)
 
 	# Helper function to check if a coordinate is within the lawn 
-	# boundaries, with padding for the robot's size.
-	# Obstacles on lawn
+	# boundaries, with padding for the robot's size. It also checks if the coordinate is colliding with any obstacles.
 	def is_inside(self, x: float, y: float, padding: float = 0.0) -> bool:
 		valid_path = True
 		#Check if robot is on lawn
 		valid_path = self.lawn.is_hitting(x, y, padding)
-		for obstacle in self.obstacles:
+		for obstacle in self.obstacles.all_obstacles:
 			if obstacle.is_hitting(x, y, padding):
 				valid_path = False
 			
@@ -54,14 +44,9 @@ class World:
 		if not self.mowed_points or self.mowed_points[-1] != point:
 			self.mowed_points.append(point)
 
-	# Main simulation loop, which also handles visualization using 
-	# Matplotlib. It utilizes FuncAnimation to update the robot's 
-	# position and the mowed path in real-time.
-	def run_simulation(self, robot: "Robot", controller: "Controller") -> None:
-		self.mark_mowed(robot.x, robot.y)
-
-		figure, axis = plt.subplots(figsize=settings.FIGURE_SIZE)
-
+	# This function sets up all static elements in the simulation like the lawn boundaries and obstacles. 
+	# It is called once at the beginning of the simulation.
+	def create_garden(self, axis: Axes):
 		axis.set_xlim(self.lawn.min_x, self.lawn.max_x)
 		axis.set_ylim(self.lawn.min_y, self.lawn.max_y)
 		axis.set_aspect("equal", adjustable="box")
@@ -72,29 +57,20 @@ class World:
 		axis.add_patch(lawn_boundary)
 		axis.add_patch(allowed_boundary)
 
-		for obstacle in self.obstacles:
-			renderd_obstacle = Rectangle(
-				(obstacle.x, obstacle.y),
-				obstacle.width,
-				obstacle.height,
-				fill=True,
-				facecolor=settings.HOUSE_COLOR,
-				edgecolor= "black",
-				linewidth=2,
-				zorder=2, 
-			)
-			obstacle_border = Rectangle(
-				(obstacle.x - settings.ROBOT_SIZE, obstacle.y - settings.ROBOT_SIZE),
-				obstacle.width + settings.ROBOT_SIZE * 2,
-				obstacle.height + settings.ROBOT_SIZE * 2,
-				fill=False,
-				edgecolor=settings.BOUNDARY_COLOR,
-  			    linewidth=2,
-  			    linestyle="--",
-  			    zorder=2,
-			)
-			axis.add_patch(renderd_obstacle)
-			axis.add_patch(obstacle_border)
+		obstacle_boundaries = self.obstacles.getPatches()
+		for obstacle in obstacle_boundaries:
+			axis.add_patch(obstacle)
+
+	# Main simulation loop, which also handles visualization using 
+	# Matplotlib. It utilizes FuncAnimation to update the robot's 
+	# position and the mowed path in real-time.
+	def run_simulation(self, robot: "Robot", controller: "Controller") -> None:
+		figure, axis = plt.subplots(figsize=settings.FIGURE_SIZE)
+		self.mark_mowed(robot.x, robot.y)
+		self.create_garden(axis)
+
+		time_legend = axis.text(0.03,0.95, f"Tid:{appState.time}", transform=axis.transAxes, fontsize=12,fontweight='bold', bbox=dict(facecolor='white',alpha=0.5))
+		goal_points = getLawnPoints(self.lawn.max_x, self.lawn.min_x, self.lawn.max_y, self.lawn.min_y, LawnPointsOption.TESTING)
 
 		# Prepares visual elements to show where the robot has mowed
 		# and where the robot currently is.
@@ -119,9 +95,6 @@ class World:
 			"draw_event",
 			lambda _event: sync_linewidth_to_data(path_line, figure, axis, blade_diameter),
 		)
-		
-		time_legend = axis.text(0.03,0.95, f"Tid:{appState.time}", transform=axis.transAxes, fontsize=12,fontweight='bold', bbox=dict(facecolor='white',alpha=0.5))
-		goal_points = getLawnPoints(self.lawn.max_x, self.lawn.min_x, self.lawn.max_y, self.lawn.min_y, LawnPointsOption.TESTING)
 
 		# This function is called by FuncAnimation during each frame.
 		# It updates the robot's position based on the controllers 
